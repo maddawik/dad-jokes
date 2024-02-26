cbb939250a246ea2dafb2c5495330fb89c716168
Mawdac <edwardreillycomputers@gmail.com>
2024-02-25 20:21:30 -0500

    refactor: separate joke collector and api logic

--- api/main.go
+++ api/main.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Server struct {
+	client *mongo.Client
+}
+
+func NewServer(c *mongo.Client) *Server {
+	return &Server{
+		client: c,
+	}
+}
+
+func (s *Server) handleGetAllJokes(w http.ResponseWriter, r *http.Request) {
+	coll := s.client.Database("dadjokes").Collection("jokes")
+
+	cursor, err := coll.Find(context.TODO(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := []bson.M{}
+
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
+
+	log.Printf("Receieved request for all jokes from %v\n", r.RemoteAddr)
+}
 
 func main() {
 	mongodb_uri := os.Getenv("MONGODB_URI")
@@ -23,11 +56,6 @@ func main() {
 		}
 	}()
 
-	worker := NewDadJokesWorker(client)
-	go worker.start()
-	// if err = worker.start(); err != nil {
-	// 	log.Fatal(err)
-	// }
 	server := NewServer(client)
 	http.HandleFunc("/jokes", server.handleGetAllJokes)
 	http.ListenAndServe(":6969", nil)
--- collector/main.go
+++ collector/main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type DadJokesWorker struct {
+	client *mongo.Client
+}
+
+func NewDadJokesWorker(c *mongo.Client) *DadJokesWorker {
+	return &DadJokesWorker{
+		client: c,
+	}
+}
+
+func (djw *DadJokesWorker) start() {
+	coll := djw.client.Database("dadjokes").Collection("jokes")
+	ticker := time.NewTicker(10 * time.Second)
+
+	req, err := http.NewRequest("GET", "https://icanhazdadjoke.com/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	client := &http.Client{}
+
+	for {
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dadJoke := bson.M{}
+
+		if err := json.NewDecoder(resp.Body).Decode(&dadJoke); err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = coll.InsertOne(context.TODO(), dadJoke)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%v\n\n", dadJoke["joke"])
+		<-ticker.C
+	}
+}
 
 func main() {
-	fmt.Println("This is a collector!")
+	mongodb_uri := os.Getenv("MONGODB_URI")
+	clientOpts := options.Client().ApplyURI(mongodb_uri)
+	client, err := mongo.Connect(context.TODO(), clientOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	worker := NewDadJokesWorker(client)
+	worker.start()
 }
