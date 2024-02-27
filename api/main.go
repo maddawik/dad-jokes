d2ee1776b3a3867c4f5fce70642ac6ffdad3f9af
Mawdac <edwardreillycomputers@gmail.com>
2024-02-26 20:59:20 -0500

    wip: add endpoint to get 1 joke

--- api/main.go
+++ api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +22,29 @@ func NewServer(c *mongo.Client) *Server {
 	return &Server{
 		client: c,
 	}
+}
+
+func (s *Server) handleGetLatestJoke(w http.ResponseWriter, r *http.Request) {
+	// var id primitive.ObjectID
+	coll := s.client.Database("dadjokes").Collection("jokes")
+	opts := options.FindOne().SetSort(bson.D{{"id", -1}})
+	var result bson.M
+	err := coll.FindOne(context.TODO(), bson.D{}, opts).Decode(&result)
+	if err != nil {
+		// ErrNoDocuments means that the filter did not match any documents in
+		// the collection.
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			fmt.Println(err)
+			return
+		}
+		log.Fatal(err)
+	}
+	fmt.Printf("found document %v", result)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+
+	log.Printf("Receieved request for latest joke from %v\n", r.RemoteAddr)
 }
 
 func (s *Server) handleGetAllJokes(w http.ResponseWriter, r *http.Request) {
@@ -58,5 +83,6 @@ func main() {
 
 	server := NewServer(client)
 	http.HandleFunc("/jokes", server.handleGetAllJokes)
+	http.HandleFunc("/joke", server.handleGetLatestJoke)
 	http.ListenAndServe(":6969", nil)
 }
